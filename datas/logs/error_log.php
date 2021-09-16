2e42320db3f332cd2a08feed9e82cbae0051a536
NCCPAAS <ncc-paas@qsncc.com>
2021-09-16 15:33:44 +0700

    first commit

---
+++ datas/logs/error_log.php
@@ -0,0 +1,69 @@
+<?php exit() ?>
+[2019-11-21 08:25:35] ERROR: <br>PHP warning : <em>fopen(/var/www/datas/cache/index.php): failed to open stream: Permission denied</em> in <b>/var/www/Kotchasan/Cache/FileCache.php</b> on line <b>57</b> 
+[2019-11-21 08:25:35] ERROR: <br>Exception : <em>File datas/cache/index.php cannot be written.</em> in <b>/var/www/Kotchasan/Database/DbCache.php</b> on line <b>76</b> 
+[2019-11-21 10:09:35] ERROR: <br>PHP warning : <em>fopen(/var/www/datas/cache/index.php): failed to open stream: Permission denied</em> in <b>/var/www/Kotchasan/Cache/FileCache.php</b> on line <b>57</b> 
+[2019-11-21 10:09:35] ERROR: <br>Exception : <em>File datas/cache/index.php cannot be written.</em> in <b>/var/www/Kotchasan/Database/DbCache.php</b> on line <b>76</b> 
+[2019-11-22 10:06:09] ERROR: <br>PHP warning : <em>fopen(/var/www/datas/cache/index.php): failed to open stream: Permission denied</em> in <b>/var/www/Kotchasan/Cache/FileCache.php</b> on line <b>57</b> 
+[2019-11-22 10:06:09] ERROR: <br>Exception : <em>File datas/cache/index.php cannot be written.</em> in <b>/var/www/Kotchasan/Database/DbCache.php</b> on line <b>76</b> 
+[2019-11-22 10:06:10] ERROR: <br>PHP warning : <em>fopen(/var/www/datas/cache/index.php): failed to open stream: Permission denied</em> in <b>/var/www/Kotchasan/Cache/FileCache.php</b> on line <b>57</b> 
+[2019-11-22 10:06:10] ERROR: <br>Exception : <em>File datas/cache/index.php cannot be written.</em> in <b>/var/www/Kotchasan/Database/DbCache.php</b> on line <b>76</b> 
+[2019-11-25 17:20:37] ERROR: <br>PHP warning : <em>fopen(/var/www/datas/cache/index.php): failed to open stream: Permission denied</em> in <b>/var/www/Kotchasan/Cache/FileCache.php</b> on line <b>57</b> 
+[2019-11-25 17:20:37] ERROR: <br>Exception : <em>File datas/cache/index.php cannot be written.</em> in <b>/var/www/Kotchasan/Database/DbCache.php</b> on line <b>76</b> 
+[2019-11-25 17:21:31] ERROR: <br>PHP warning : <em>fopen(/var/www/datas/cache/index.php): failed to open stream: Permission denied</em> in <b>/var/www/Kotchasan/Cache/FileCache.php</b> on line <b>57</b> 
+[2019-11-25 17:21:31] ERROR: <br>Exception : <em>File datas/cache/index.php cannot be written.</em> in <b>/var/www/Kotchasan/Database/DbCache.php</b> on line <b>76</b> 
+[2019-11-25 17:21:33] ERROR: <br>PHP warning : <em>fopen(/var/www/datas/cache/index.php): failed to open stream: Permission denied</em> in <b>/var/www/Kotchasan/Cache/FileCache.php</b> on line <b>57</b> 
+[2019-11-25 17:21:33] ERROR: <br>Exception : <em>File datas/cache/index.php cannot be written.</em> in <b>/var/www/Kotchasan/Database/DbCache.php</b> on line <b>76</b> 
+[2019-11-25 17:21:50] ERROR: <br>PHP warning : <em>fopen(/var/www/datas/cache/index.php): failed to open stream: Permission denied</em> in <b>/var/www/Kotchasan/Cache/FileCache.php</b> on line <b>57</b> 
+[2019-11-25 17:21:50] ERROR: <br>Exception : <em>File datas/cache/index.php cannot be written.</em> in <b>/var/www/Kotchasan/Database/DbCache.php</b> on line <b>76</b> 
+[2019-11-25 17:21:55] ERROR: <br>PHP warning : <em>fopen(/var/www/datas/cache/index.php): failed to open stream: Permission denied</em> in <b>/var/www/Kotchasan/Cache/FileCache.php</b> on line <b>57</b> 
+[2019-11-25 17:21:55] ERROR: <br>Exception : <em>File datas/cache/index.php cannot be written.</em> in <b>/var/www/Kotchasan/Database/DbCache.php</b> on line <b>76</b> 
+[2019-11-25 17:22:37] ERROR: <br>PHP warning : <em>fopen(/var/www/datas/cache/index.php): failed to open stream: Permission denied</em> in <b>/var/www/Kotchasan/Cache/FileCache.php</b> on line <b>57</b> 
+[2019-11-25 17:22:37] ERROR: <br>Exception : <em>File datas/cache/index.php cannot be written.</em> in <b>/var/www/Kotchasan/Database/DbCache.php</b> on line <b>76</b> 
+[2019-11-25 17:22:43] ERROR: <br>PHP warning : <em>fopen(/var/www/datas/cache/index.php): failed to open stream: Permission denied</em> in <b>/var/www/Kotchasan/Cache/FileCache.php</b> on line <b>57</b> 
+[2019-11-25 17:22:43] ERROR: <br>Exception : <em>File datas/cache/index.php cannot be written.</em> in <b>/var/www/Kotchasan/Database/DbCache.php</b> on line <b>76</b> 
+[2019-11-25 17:22:48] ERROR: <br>PHP warning : <em>fopen(/var/www/datas/cache/index.php): failed to open stream: Permission denied</em> in <b>/var/www/Kotchasan/Cache/FileCache.php</b> on line <b>57</b> 
+[2019-11-25 17:22:48] ERROR: <br>Exception : <em>File datas/cache/index.php cannot be written.</em> in <b>/var/www/Kotchasan/Database/DbCache.php</b> on line <b>76</b> 
+[2019-11-25 17:23:14] ERROR: <br>PHP warning : <em>fopen(/var/www/datas/cache/index.php): failed to open stream: Permission denied</em> in <b>/var/www/Kotchasan/Cache/FileCache.php</b> on line <b>57</b> 
+[2019-11-25 17:23:14] ERROR: <br>Exception : <em>File datas/cache/index.php cannot be written.</em> in <b>/var/www/Kotchasan/Database/DbCache.php</b> on line <b>76</b> 
+[2019-11-25 17:23:18] ERROR: <br>PHP warning : <em>fopen(/var/www/datas/cache/index.php): failed to open stream: Permission denied</em> in <b>/var/www/Kotchasan/Cache/FileCache.php</b> on line <b>57</b> 
+[2019-11-25 17:23:18] ERROR: <br>Exception : <em>File datas/cache/index.php cannot be written.</em> in <b>/var/www/Kotchasan/Database/DbCache.php</b> on line <b>76</b> 
+[2019-11-25 17:24:46] ERROR: <br>PHP warning : <em>fopen(/var/www/datas/cache/index.php): failed to open stream: Permission denied</em> in <b>/var/www/Kotchasan/Cache/FileCache.php</b> on line <b>57</b> 
+[2019-11-25 17:24:46] ERROR: <br>Exception : <em>File datas/cache/index.php cannot be written.</em> in <b>/var/www/Kotchasan/Database/DbCache.php</b> on line <b>76</b> 
+[2019-11-25 17:26:52] ERROR: <br>PHP warning : <em>fopen(/var/www/datas/cache/index.php): failed to open stream: Permission denied</em> in <b>/var/www/Kotchasan/Cache/FileCache.php</b> on line <b>57</b> 
+[2019-11-25 17:26:52] ERROR: <br>Exception : <em>File datas/cache/index.php cannot be written.</em> in <b>/var/www/Kotchasan/Database/DbCache.php</b> on line <b>76</b> 
+[2019-11-25 17:26:54] ERROR: <br>PHP warning : <em>fopen(/var/www/datas/cache/index.php): failed to open stream: Permission denied</em> in <b>/var/www/Kotchasan/Cache/FileCache.php</b> on line <b>57</b> 
+[2019-11-25 17:26:54] ERROR: <br>Exception : <em>File datas/cache/index.php cannot be written.</em> in <b>/var/www/Kotchasan/Database/DbCache.php</b> on line <b>76</b> 
+[2019-11-25 17:27:53] ERROR: <br>PHP warning : <em>fopen(/var/www/datas/cache/index.php): failed to open stream: Permission denied</em> in <b>/var/www/Kotchasan/Cache/FileCache.php</b> on line <b>57</b> 
+[2019-11-25 17:27:53] ERROR: <br>Exception : <em>File datas/cache/index.php cannot be written.</em> in <b>/var/www/Kotchasan/Database/DbCache.php</b> on line <b>76</b> 
+[2019-11-25 17:28:05] ERROR: <br>PHP warning : <em>fopen(/var/www/datas/cache/index.php): failed to open stream: Permission denied</em> in <b>/var/www/Kotchasan/Cache/FileCache.php</b> on line <b>57</b> 
+[2019-11-25 17:28:05] ERROR: <br>Exception : <em>File datas/cache/index.php cannot be written.</em> in <b>/var/www/Kotchasan/Database/DbCache.php</b> on line <b>76</b> 
+[2019-11-28 09:06:51] ERROR: <br>PHP warning : <em>fopen(/var/www/datas/cache/index.php): failed to open stream: Permission denied</em> in <b>/var/www/Kotchasan/Cache/FileCache.php</b> on line <b>57</b> 
+[2019-11-28 09:06:51] ERROR: <br>Exception : <em>File datas/cache/index.php cannot be written.</em> in <b>/var/www/Kotchasan/Database/DbCache.php</b> on line <b>76</b> 
+[2019-11-28 09:13:13] ERROR: <br>PHP warning : <em>fopen(/var/www/datas/cache/index.php): failed to open stream: Permission denied</em> in <b>/var/www/Kotchasan/Cache/FileCache.php</b> on line <b>57</b> 
+[2019-11-28 09:13:13] ERROR: <br>Exception : <em>File datas/cache/index.php cannot be written.</em> in <b>/var/www/Kotchasan/Database/DbCache.php</b> on line <b>76</b> 
+[2019-11-28 09:13:15] ERROR: <br>PHP warning : <em>fopen(/var/www/datas/cache/index.php): failed to open stream: Permission denied</em> in <b>/var/www/Kotchasan/Cache/FileCache.php</b> on line <b>57</b> 
+[2019-11-28 09:13:15] ERROR: <br>Exception : <em>File datas/cache/index.php cannot be written.</em> in <b>/var/www/Kotchasan/Database/DbCache.php</b> on line <b>76</b> 
+[2019-11-28 09:13:18] ERROR: <br>PHP warning : <em>fopen(/var/www/datas/cache/index.php): failed to open stream: Permission denied</em> in <b>/var/www/Kotchasan/Cache/FileCache.php</b> on line <b>57</b> 
+[2019-11-28 09:13:18] ERROR: <br>Exception : <em>File datas/cache/index.php cannot be written.</em> in <b>/var/www/Kotchasan/Database/DbCache.php</b> on line <b>76</b> 
+[2019-11-28 09:13:21] ERROR: <br>PHP warning : <em>fopen(/var/www/datas/cache/index.php): failed to open stream: Permission denied</em> in <b>/var/www/Kotchasan/Cache/FileCache.php</b> on line <b>57</b> 
+[2019-11-28 09:13:21] ERROR: <br>Exception : <em>File datas/cache/index.php cannot be written.</em> in <b>/var/www/Kotchasan/Database/DbCache.php</b> on line <b>76</b> 
+[2019-11-28 09:13:22] ERROR: <br>PHP warning : <em>fopen(/var/www/datas/cache/index.php): failed to open stream: Permission denied</em> in <b>/var/www/Kotchasan/Cache/FileCache.php</b> on line <b>57</b> 
+[2019-11-28 09:13:22] ERROR: <br>Exception : <em>File datas/cache/index.php cannot be written.</em> in <b>/var/www/Kotchasan/Database/DbCache.php</b> on line <b>76</b> 
+[2019-12-17 00:15:16] ERROR: <br>PHP warning : <em>PDO::__construct(): MySQL server has gone away</em> in <b>/var/code/booking/Kotchasan/Database/PdoMysqlDriver.php</b> on line <b>59</b> 
+[2019-12-18 09:18:55] ERROR: <br>PHP notice : <em>PDO::__construct(): send of 5 bytes failed with errno=32 Broken pipe</em> in <b>/var/code/booking/Kotchasan/Database/PdoMysqlDriver.php</b> on line <b>59</b> 
+[2019-12-18 09:18:56] ERROR: <br>PHP notice : <em>PDO::__construct(): send of 5 bytes failed with errno=32 Broken pipe</em> in <b>/var/code/booking/Kotchasan/Database/PdoMysqlDriver.php</b> on line <b>59</b> 
+[2019-12-18 16:08:23] ERROR: <br>Exception : <em>SQLSTATE[42S02]: Base table or view not found: 1146 Table 'booking.booking_reservation' doesn't exist</em> in <b>/var/code/booking/Kotchasan/Database/Driver.php</b> on line <b>246</b> 
+[2019-12-18 16:09:00] ERROR: <br>Exception : <em>SQLSTATE[42S02]: Base table or view not found: 1146 Table 'booking.booking_reservation' doesn't exist</em> in <b>/var/code/booking/Kotchasan/Database/Driver.php</b> on line <b>246</b> 
+[2019-12-18 16:09:02] ERROR: <br>Exception : <em>SQLSTATE[42S02]: Base table or view not found: 1146 Table 'booking.booking_reservation' doesn't exist</em> in <b>/var/code/booking/Kotchasan/Database/Driver.php</b> on line <b>246</b> 
+[2019-12-18 16:09:03] ERROR: <br>Exception : <em>SQLSTATE[42S02]: Base table or view not found: 1146 Table 'booking.booking_reservation' doesn't exist</em> in <b>/var/code/booking/Kotchasan/Database/Driver.php</b> on line <b>246</b> 
+[2019-12-18 16:09:04] ERROR: <br>Exception : <em>SQLSTATE[42S02]: Base table or view not found: 1146 Table 'booking.booking_reservation' doesn't exist</em> in <b>/var/code/booking/Kotchasan/Database/Driver.php</b> on line <b>246</b> 
+[2019-12-18 16:09:13] ERROR: <br>Exception : <em>SQLSTATE[42S02]: Base table or view not found: 1146 Table 'booking.booking_reservation' doesn't exist</em> in <b>/var/code/booking/Kotchasan/Database/Driver.php</b> on line <b>246</b> 
+[2019-12-18 16:11:26] ERROR: <br>Exception : <em>SQLSTATE[42S02]: Base table or view not found: 1146 Table 'booking.booking_reservation' doesn't exist</em> in <b>/var/code/booking/Kotchasan/Database/Driver.php</b> on line <b>246</b> 
+[2019-12-24 17:46:49] ERROR: <br>PHP warning : <em>fopen(/var/www/html/booking/datas/cache/4c641eece804f2ecc4c5de3e780c452e.php): failed to open stream: Permission denied</em> in <b>/var/www/html/booking/Kotchasan/Cache/FileCache.php</b> on line <b>150</b> 
+[2019-12-24 17:46:49] ERROR: <br>Exception : <em>resource cache file cannot be created.</em> in <b>/var/www/html/booking/Kotchasan/Database/PdoMysqlDriver.php</b> on line <b>362</b> 
+[2020-07-22 09:30:38] ERROR: <br>PHP warning : <em>session_start(): open(/var/lib/php/sessions/sess_5d9ff6cf59e1c5a336318e4195c3ea0c, O_RDWR) failed: No such file or directory (2)</em> in <b>/var/www/html/booking/Kotchasan/Http/Request.php</b> on line <b>300</b> 
+[2020-07-22 09:30:38] ERROR: <br>PHP warning : <em>session_start(): Failed to read session data: files (path: /var/lib/php/sessions)</em> in <b>/var/www/html/booking/Kotchasan/Http/Request.php</b> on line <b>300</b> 
+[2020-07-22 09:30:39] ERROR: <br>PHP warning : <em>session_start(): open(/var/lib/php/sessions/sess_5d9ff6cf59e1c5a336318e4195c3ea0c, O_RDWR) failed: No such file or directory (2)</em> in <b>/var/www/html/booking/Kotchasan/Http/Request.php</b> on line <b>300</b> 
+[2020-07-22 09:30:39] ERROR: <br>PHP warning : <em>session_start(): Failed to read session data: files (path: /var/lib/php/sessions)</em> in <b>/var/www/html/booking/Kotchasan/Http/Request.php</b> on line <b>300</b> 
+[2020-07-22 09:30:39] ERROR: <br>PHP warning : <em>session_start(): open(/var/lib/php/sessions/sess_5d9ff6cf59e1c5a336318e4195c3ea0c, O_RDWR) failed: No such file or directory (2)</em> in <b>/var/www/html/booking/Kotchasan/Http/Request.php</b> on line <b>300</b> 
+[2020-07-22 09:30:39] ERROR: <br>PHP warning : <em>session_start(): Failed to read session data: files (path: /var/lib/php/sessions)</em> in <b>/var/www/html/booking/Kotchasan/Http/Request.php</b> on line <b>300</b> 
